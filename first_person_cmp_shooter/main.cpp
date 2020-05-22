2028264e426fdbe150ad99852ac88c81f8e8849a
dmg59912 <dmg138380@yahoo.com>
2020-05-22 03:01:21 -0700

    added movement and wall detection

--- first_person_cmp_shooter/main.cpp
+++ first_person_cmp_shooter/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<chrono>
 #include<Windows.h>
 
 //using namespace std;
@@ -20,7 +21,7 @@ int map_width = 16;
 
 
 // to detect if we are near a wall or far 
-float ffo_view = 3.14156/4;
+float ffo_view = 3.14156 / 4;
 float fDepth = 16;
 int main(void)
 {
@@ -33,25 +34,73 @@ int main(void)
 
 	std::wstring map;
 
-	map +=L"################";
+	map += L"################";
+	map += L"#..............#";
+	map += L"#..###.........#";
 	map += L"#..............#";
 	map += L"#..............#";
 	map += L"#..............#";
 	map += L"#..............#";
+	map += L"#......###.....#";
 	map += L"#..............#";
 	map += L"#..............#";
 	map += L"#..............#";
 	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
+	map += L"#.......########";
 	map += L"#..............#";
 	map += L"#..............#";
 	map += L"################";
 
+
+	// to adjust our frame buffer with the system 
+	auto tp1 = std::chrono::system_clock::now();
+	auto tp2 = std::chrono::system_clock::now();
+
 	while (true)
 	{
+		//move each frame with our input 
+		tp2 = std::chrono::system_clock::now();
+		std::chrono::duration<float> elapsedTime = tp2 - tp1;
+		tp1 = tp2;
+		float fElapsedTime = elapsedTime.count();
+
+		//controls
+		//handle CCW Rotaion 
+		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
+			playerA -= (0.8f) * fElapsedTime;
+
+		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
+			playerA += .8f * fElapsedTime;
+
+		//movement forward
+		if (GetAsyncKeyState((unsigned short)'W') & 0x8000)
+		{
+			player_x_position += sinf(playerA) * 5.0f * fElapsedTime;
+			player_y_position += cosf(playerA) * 5.0f * fElapsedTime;
+
+			//to detect coallition, if it hits a '#' then it will undo the move giving the ellusion that it cannot move pass the wall
+			if (map[(int)player_y_position * map_width + (int)player_x_position] == '#')
+			{
+				player_x_position -= sinf(playerA) * 5.0f * fElapsedTime;
+				player_y_position -= cosf(playerA) * 5.0f * fElapsedTime;
+			}
+
+		}
+
+		if (GetAsyncKeyState((unsigned short)'S') & 0x8000)
+		{
+			player_x_position -= sinf(playerA) * 5.0f * fElapsedTime;
+			player_y_position -= cosf(playerA) * 5.0f * fElapsedTime;
+
+			//to detect coallition, if it hits a '#' then it will undo the move giving the ellusion that it cannot move pass the wall
+			if (map[(int)player_y_position * map_width + (int)player_x_position] == '#')
+			{
+				player_x_position += sinf(playerA) * 5.0f * fElapsedTime;
+				player_y_position += cosf(playerA) * 5.0f * fElapsedTime;
+			}
+		}
+	
+
 		for (int x = 0; x < n_screen_width; x++)
 		{
 			//for each column, calculate the projected ray angle into world space
@@ -90,14 +139,42 @@ int main(void)
 			int ceiling = (float)(n_screen_height / 2.0) - n_screen_height / ((float)f_distance_to_wall);
 			int floor = n_screen_height - ceiling;
 
+			short nShade = ' ';
+
+			if (f_distance_to_wall <= fDepth / 4.0f)
+				nShade = 0x2588; // very close to the wall
+			else if (f_distance_to_wall < fDepth / 3.0f)
+				nShade = 0x2593;
+			else if (f_distance_to_wall < fDepth / 2.0f)
+				nShade = 0x2592;
+			else if (f_distance_to_wall < fDepth)
+				nShade = 0x2591;
+			else
+				nShade = ' '; // too far
+
 			for (int y = 0; y < n_screen_height; y++)
 			{
 				if (y < ceiling)
 					screen[y * n_screen_width + x] = ' ';
-				else if (y> ceiling && y <= floor)
-					screen[y * n_screen_width + x] = '#';
+				else if (y > ceiling && y <= floor)
+					screen[y * n_screen_width + x] = nShade;
 				else
-				screen[y * n_screen_width + x] = ' ';
+				{
+					short fShade = ' ';
+					//shade floor based on distance 
+					float b = 1.0f - (((float)y - n_screen_height / 2.0f) / ((float)n_screen_height / 2.0f));
+					if (b < 0.25)
+						fShade = '#';
+					else if (b < 0.5)
+						fShade = 'x';
+					else if (b < 0.75)
+						fShade = '.';
+					else if (b < 0.9)
+						fShade = '-';
+					else
+						fShade = ' ';
+					screen[y * n_screen_width + x] = fShade;
+				}
 			}
 		}
 		//set last value in our array with the escape character to know when to stop 
