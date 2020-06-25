c0feb072f2edb240f9c82171550a39be540656c4
dmg59912 <dmg138380@yahoo.com>
2020-06-24 22:50:42 -0700

    rotate image update

--- possible_questions/rotate_image.cpp
+++ possible_questions/rotate_image.cpp
@@ -1,10 +1,42 @@
-#include<iostream>
+﻿#include<iostream>
 #include<vector>
+/*
+Note: Try to solve this task in-place (with O(1) additional memory), since this is what you'll be asked to do during an interview.
+
+You are given an n x n 2D matrix that represents an image. Rotate the image by 90 degrees (clockwise).
+
+Example
+
+For
+
+a = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+the output should be
+
+rotateImage(a) =
+    [[7, 4, 1],
+     [8, 5, 2],
+     [9, 6, 3]]
+Input/Output
+
+[execution time limit] 0.5 seconds (cpp)
+
+[input] array.array.integer a
+
+Guaranteed constraints:
+1 ≤ a.length ≤ 100,
+a[i].length = a.length,
+1 ≤ a[i][j] ≤ 104.
+*/
+
 
 
 
 std::vector<std::vector<int>> rotateImage(std::vector<std::vector<int>> & a);
 void print_matrix(std::vector<std::vector<int>>);
+
+
 int main(void)
 {
     std::vector<std::vector<int>> matrix = { {1,2,3 }, {4, 5, 6}, {7,8,9} };
@@ -36,6 +68,20 @@ std::vector<std::vector<int>> rotateImage(std::vector<std::vector<int>> & a)
 
     //step 1, we need to transpose the matrix ( turn rows to columns)
 
+    /*
+
+        ex. 
+            a = [[1, 2, 3],
+                 [4, 5, 6],
+                 [7, 8, 9]]
+
+                   transpose or swap(array[i][j], array[j][i];
+    
+                 [[1, 4, 7 ],
+                  [2, 5, 8 ],
+                  [3, 6, 9]]
+    */
+
     int n = a.size();
 
     for (int i = 0; i < n; ++i)
@@ -49,6 +95,20 @@ std::vector<std::vector<int>> rotateImage(std::vector<std::vector<int>> & a)
     }
 
     // step 2. flip horizontaly 
+      /*
+
+        ex.
+            a = [[1, 2, 3],
+                 [4, 5, 6],
+                 [7, 8, 9]]
+
+                  flip horizontally or swap(array[i][j], array[i][n -1 -j];
+
+                 [[7, 4, 1 ],
+                  [8, 5, 2 ],
+                  [9, 6, 3]]
+    */
+
     for (int i = 0; i < n; ++i)
     {
         for (int j = 0; j < (n / 2); ++j)
